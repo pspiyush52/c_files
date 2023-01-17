338380045f48cd4e86c450d465f2cfd7aa9ceb91
Piyush Singh <pspiyush52@gmail.com>
2023-01-17 18:01:06 +0530

    ASD

--- string_strip.c
+++ string_strip.c
@@ -4,7 +4,6 @@
 #include <ctype.h>
 #define N 50
 
-char* stcat(char* s1, char* s2);
 char* strip(char* s);
 
 int main(void)
@@ -15,24 +14,9 @@ int main(void)
     system("cls");
     strip(s);
     if (isupper(s[0]))
-        fputs(stcat(s, " Singh"), stdout);
+        fputs(strcat(s, " Singh"), stdout);
     else
-        fputs(stcat(s, " singh"), stdout);
-}
-
-char* stcat(char* s1 ,char* s2)
-{
-    int n1 = strlen(s1), n2 = strlen(s2);
-    int n = n1 + n2;
-    char* st = malloc((n+1) * sizeof(char));
-    for (int i=0; i<n1; i++)
-        st[i] = s1[i];
-    
-    for (int i=n1, j=0; i<n; j++, i++)
-        st[i] = s2[j];
-
-    st[n] = '\0';
-    return st;
+        fputs(strcat(s, " singh"), stdout);
 }
 
 char* strip(char* s)
