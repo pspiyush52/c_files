6ef7693117cf539468896cc215f4933274c104bc
Piyush Singh <pspiyush52@gmail.com>
2023-01-18 10:40:59 +0530

    asd

--- trials.c
+++ trials.c
@@ -6,7 +6,13 @@ void endl();
 
 int main(void)
 {
-    
+    char s[50];
+    // fgets(s, 50, stdin);
+    gets(s);
+    // fputs(s, stdout);
+    // printf("%s", s);
+    puts(s);
+    printf("--ENDL--");
 }
 
 void endl()
