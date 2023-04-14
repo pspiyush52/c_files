1a07b4de5823431e6edc76f4f9ac3d9d0421217e
Piyush Singh <pspiyush52@gmail.com>
2023-04-15 03:11:13 +0530

    asd

--- trials.c
+++ trials.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#define print printf
 
 int main(void)
 {
-    printf("\n");
+    print("\n");
     for (int i = 1; i <= 10; i++)
     {
         for (int j = 0; j < 10; j++)
-            printf("%5d", 10*j + i);
-        printf("\n\n");
+            print("%5d", 10*j + i);
+        print("\n\n");
     }
 
     return 0;
 }
+
+
+// 1 11 21 31 ...
+// 2 12 22 32 ...
+// n     10*i + n      10*i + n
